bdd520e1c5ae48843377da701bb96a3308be9116
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2019-12-16 01:02:39 +0100

    Fixed bug in javadocs.

--- src/main/java/com/aparapi/natives/util/NativeUtils.java
+++ src/main/java/com/aparapi/natives/util/NativeUtils.java
@@ -40,7 +40,7 @@ public class NativeUtils {
      * @param path The path of file inside JAR as absolute path (beginning with '/'), e.g. /package/File.ext
      * @throws IOException If temporary file creation or read/write operation fails
      * @throws IllegalArgumentException If source file (param path) does not exist
-     * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters (restriction of {@see File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters (restriction of @see File#createTempFile(java.lang.String, java.lang.String)).
      */
     public static void loadLibraryFromJar(String path) throws IOException {
 
