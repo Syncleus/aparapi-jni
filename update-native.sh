251ccaf25f5cf073b8761a1dec3247b42efb299b
Jeffrey Phillips Freeman <the@jeffreyfreeman.me>
2021-02-14 16:25:15 -0500

    Added script to compile native libraries and copy them over to the repo to prepare for a release.

---
+++ update-native.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd native
+UID=${UID} GID=${GID} docker-compose run --rm aparapi-native-builder
+cd ..
+
+cp -f ./native/.libs32/win/libgcc_s_sjlj-1.dll ./src/main/resources/win/libgcc_s_sjlj_x86.dll
+cp -f ./native/.libs32/win/libstdc++-6.dll ./src/main/resources/win/libstdc++-6_x86.dll
+cp -f ./native/.libs32/win/libaparapi-1.dll ./src/main/resources/win/libaparapi_x86.dll
+
+cp -f ./native/.libs64/win/libgcc_s_seh-1.dll ./src/main/resources/win/libgcc_s_seh_x86_64.dll
+cp -f ./native/.libs64/win/libstdc++-6.dll ./src/main/resources/win/libstdc++-6_x86_64.dll
+cp -f ./native/.libs64/win/libaparapi-1.dll ./src/main/resources/win/libaparapi_x86_64.dll
+
+cp -f ./native/.libs32/linux/libaparapi.so.*.*.* ./src/main/resources/linux/libaparapi_x86.so
+cp -f ./native/.libs64/linux/libaparapi.so.*.*.* ./src/main/resources/linux/libaparapi_x86_64.so
+
+cp -f ./native/.libs32/linuxARM/libaparapi.so.*.*.* ./src/main/resources/linux/libaparapi_armhf.so
+cp -f ./native/.libs64/linuxARM/libaparapi.so.*.*.* ./src/main/resources/linux/libaparapi_aarch64.so
